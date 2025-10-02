34c3399d6428c04df7fcab4aca1563acd73fca96
Aaron  Lucas <aaronlucas@aarons-mbp-2.mynetworksettings.com>
2025-10-01 21:15:10 -0400

    added api function

--- script.js
+++ script.js
@@ -3,42 +3,52 @@ function progressBar(rootEl, percent, slider) {
     alert("Please enter a valid number");
     return;
   }
-
-  const min = 0;
-  const max = 100;
-  const value = Math.min(Math.max(percent, min), max);
-
+  let sliderEl;
   const progressBar = document.createElement("div");
   progressBar.classList.add("progress-bar");
   progressBar.setAttribute("role", "progressbar");
   progressBar.id = `progress-${percent}`;
   progressBar.setAttribute("aria-valuemin", 0);
   progressBar.setAttribute("aria-valuemax", 100);
-  progressBar.setAttribute("aria-valuenow", value);
 
   const progressAmount = document.createElement("div");
   progressAmount.classList.add("progress-bar--fill");
-  progressAmount.textContent = `${value}%`;
-  progressAmount.style.width = `${value}%`;
-
   progressBar.append(progressAmount);
 
+  function setValue(value) {
+    const min = 0;
+    const max = 100;
+    const cValue = Math.min(Math.max(value, min), max);
+    progressBar.setAttribute("aria-valuenow", cValue);
+    progressAmount.textContent = `${cValue}%`;
+    progressAmount.style.width = `${cValue}%`;
+
+    if (sliderEl) sliderEl.value = String(cValue);
+    return cValue;
+  }
+
+  const initial = setValue(percent);
+
   if (slider) {
-    const sliderEl = document.createElement("input");
+    sliderEl = document.createElement("input");
     sliderEl.setAttribute("type", "range");
     sliderEl.setAttribute("min", "0");
     sliderEl.setAttribute("max", "100");
-    sliderEl.setAttribute("value", `${value}`);
+    sliderEl.setAttribute("value", String(initial));
     rootEl.append(progressBar, sliderEl);
 
     sliderEl.addEventListener("input", (e) => {
-      progressAmount.style.width = `${sliderEl.value}%`;
-      progressBar.setAttribute("aria-valuenow", `${sliderEl.value}`);
-      progressAmount.textContent = `${sliderEl.value}%`;
+      setValue(sliderEl.value);
     });
   } else {
     rootEl.append(progressBar);
   }
+
+  return {
+    setValue,
+  };
 }
 
-progressBar(document.querySelector(".wrapper"), -1000, true);
+const bar1 = progressBar(document.querySelector(".wrapper"), -1000, true);
+bar1.setValue(30);
+bar1.setValue(50);
